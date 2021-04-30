44d19099ae3cf63785de6884cb9983453173931d
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-04-30 17:00:27 +0300

    Associations fixed and faker gem installed for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +6,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-airpotrs = ["ATL", "DFW", "DEN", "ORD", "LAX", "CLT", "IAH", "LAS", "MCO"]
+# airports = ["ATL", "DFW", "DEN", "ORD", "LAX", "CLT", "IAH", "LAS", "MCO"]
 
-airports.each do |a|
-  Airport.create(code: a)
+# airports.each do |a|
+#   Airport.create(code: a)
+# end
+
+25.times do
+  Flight.create(
+    start: Faker::Time.forward(days: 365, period: :all, format: :short),
+    duration: "#{Faker::Number.between(from: 2, to: 10)} hours",
+    from_airport_id: Faker::Number.between(from: 1, to: 9),
+    to_airport_id: Faker::Number.between(from: 1, to: 9)
+  )
 end
+
