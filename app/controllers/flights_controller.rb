3a4cc85fd3f1794c135349145cb899a036ecc648
Taras <ivanyuk.taras@gmail.com>
2023-05-22 20:21:06 -0500

    Refactor flights_controller.rb

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,12 @@
 class FlightsController < ApplicationController
-  
   def index
     @dates = Flight.order(:start).pluck(:start).map { |a| a.strftime("%Y-%m-%d")}.uniq
-    if !params.empty?
-      @flights = Flight.order(:start).where("from_airport_id = ?", 
-        params[:from_airport_id]).where("to_airport_id = ?", params[:to_airport_id]).where("DATE(start) >= ? AND DATE(start) <= ?", 
-        params[:start], params[:start])
 
+    if !params.empty?
+      @flights = Flight.order(:start)
+                       .where(from_airport_id: params[:from_airport_id])
+                       .where(to_airport_id: params[:to_airport_id])
+                       .where("DATE(start) >= ? AND DATE(start) <= ?", params[:start], params[:start])
     end 
   end
-
 end
